78b014706fbf79bf80ca194a1217e38c6ce3adfb
Imre Gulyas <imgulyas@gmail.com>
2020-12-10 15:47:19 +0100

    initial commit

---
+++ src/Aoc.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2020 Imre Gulyas
+SPDX-License-Identifier: MIT
+Maintainer: Imre Gulyas <imgulyas@gmail.com>
+
+See README for more info
+-}
+
+module Aoc
+       ( someFunc
+       ) where
+
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
